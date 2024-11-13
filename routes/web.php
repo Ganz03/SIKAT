814b48a1827e188a0d20bbb647d1b056c4e62d79
Ahmad Fahrezi <ahmadfahrezir@gmail.com>
2024-11-13 20:03:49 +0700

    mengubah mahasiswa

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IRSController;
+use App\Http\Controllers\KHSController;
+use App\Http\Controllers\BuatIrsController;
 
 Route::get('/', function () {
     return view('dashboard');
 });
+
+
+Route::get('/dashboard-mahasiswa', function () {
+    return view('mahasiswa.dashboard-mahasiswa');
+});
+
+Route::get('/buat_irs', [BuatIrsController::class, 'index'])->name('buat_irs');
+
+Route::get('/irs', [IRSController::class, 'index'])->name('irs');
+
+Route::get('/khs', [KHSController::class, 'index'])->name('khs');
